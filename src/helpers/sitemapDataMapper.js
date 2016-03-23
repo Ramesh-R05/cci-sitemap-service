f64d25ecca14cd12cdf7646859f74b05934f8620
Min Park <mpark@bauerxcelmedia.com.au>
2016-03-23 17:08:39 +1100

    Scaffold: Workflow

--- src/helpers/sitemapDataMapper.js
+++ src/helpers/sitemapDataMapper.js
@@ -4,21 +4,20 @@ function extractDataToSave(source, siteId) {
     return {
         id: source.id,
         siteId: siteId,
-
         data: {
             nodeTypeAlias: source.nodeTypeAlias,                // sitemap index node will have "GoogleSitemap"
             siteUrl: source.siteUrl,                            // root url
             url: source.url,                                    // sub url. <loc> = siteUrl + url
             urlName: source.urlName,
-            sitemapFrequency: source.sitemapFrequency,          // should be used for <changefreq> in sub sitemap
-            sitemapPriority: source.sitemapPriority,            // should be used for <priority> in sub sitemap
-            sitemapRootNodeId: source.sitemapRootNodeId,        //section id for the index
-            path: source.path,                                  //To manipulate sub sitemap, this field needs to be filtered by sitempatRootNodeId
-            pageDateCreated: source.pageDateCreated,            //<lastmod> in sub sitemap
-            contentImageUrl: source.contentImageUrl,            //<image:loc> in sub sitemap (not news)
-            contentTitle: source.contentTitle,                  //<image:title> in sub sitemap
-            siteTitle: source.siteTitle,                        //<news:name> in news sitemap
-            contentNewsKeywords: source.contentNewsKeywords,    //<news:keywords> in news sitemap
+            sitemapFrequency: source.sitemapFrequency,          // <changefreq>
+            sitemapPriority: source.sitemapPriority,            // <priority>
+            sitemapRootNodeId: source.sitemapRootNodeId,        // section id for the index
+            path: source.path,
+            pageDateCreated: source.pageDateCreated,            // <lastmod>
+            contentImageUrl: source.contentImageUrl,            // <image:loc>
+            contentTitle: source.contentTitle,                  // <image:title>
+            siteTitle: source.siteTitle,                        // <news:name>
+            contentNewsKeywords: source.contentNewsKeywords,    // <news:keywords>
         }
     };
 }
