3b914254cad721be1bd360f84fbe1cbf61f0bc16
Abhibxm <asharma@bauerxcelmedia.com.au>
2017-09-14 11:42:38 +1000

    Feature/sitemap migration (#29)
    * Adde apm, logger files and newrelic npm package
    
    * Corrected the files

--- src/app/server.js
+++ src/app/server.js
@@ -2,13 +2,18 @@ import Server from '@bxm/microservice';
 import routes from './routes';
 import version from './../version';
 import messageController from './controllers/messageController';
+import config from './config';
+import logger from './../logger';
+
 
 export default function() {
     const server = new Server({
         name: 'SITEMAP_SERVICE',
         version: version,
         docs: './docs/api.raml',
-        routes: routes
+        routes: routes,
+        config: config,
+        logger: logger
     });
 
     messageController.subscribe();
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-'use strict';
-require('@bxm/node-apm')('sitemap service', 'service');
+process.env.APP_KEY = 'sitemap';
+process.title = process.env.APP_KEY;
+
 require('babel/register')();
+require('./logger');
+require('./apm');
 
 module.exports = require('./app/server')();
